0dc8f3f83353726bb4f89fe957187b2d593edba6
persiandarik <persiandarik@gmail.com>
2026-02-09 15:05:43 +0330

    [ADD] useState

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
-import type { ReactNode } from "react";
+import { type ReactNode, useState } from "react";
 
 import "./App.css";
 
 export default function App(): ReactNode {
-  return <div>Hello, friend!</div>;
+  const [count, setCount] = useState(0);
+
+  const increment = (): void => {
+    setCount(count + 1);
+  };
+
+  return (
+    <div>
+      <div>{count}</div>
+      <button onClick={increment}>Increment</button>
+    </div>
+  );
 }
+
+// 1. Changes to Local Variables Won't Trigger Renders
+// 2. Local variables Don't Persist Between Renders
