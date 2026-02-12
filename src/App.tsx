3e380847e725c1dc3bcac79803ddc4169dc0dbd8
persiandarik <persiandarik@gmail.com>
2026-02-12 12:08:25 +0330

    [ADD]  component

--- src/App.tsx
+++ src/App.tsx
@@ -3,25 +3,28 @@ import { type ReactNode, useState } from "react";
 import "./App.css";
 
 export default function App(): ReactNode {
-  const [numbers, setNumbers] = useState<number[]>([
-    1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
-  ]);
-
-  const handleButtonClick = (): void => {
-    setNumbers((old) => {
-      const newNumbers = [...old];
-      newNumbers.splice(3, 1);
-      return newNumbers;
-    });
-  };
-
   return (
-    <div>
-      <pre>{JSON.stringify(numbers, null, 2)}</pre>
-      <button onClick={handleButtonClick}>Remove Index 3</button>
+    <div className="app">
+      <Counter />
+      <Counter />
     </div>
   );
 }
 
-// 1. Changes to Local Variables Won't Trigger Renders
-// 2. Local variables Don't Persist Between Renders
+function Counter(): ReactNode {
+  const [count, setCount] = useState<number>(0);
+
+  const handleButtonClick = (): void => {
+    setCount((old) => old + 1);
+  };
+
+  return (
+    <div className="counter">
+      <div className="title">Title</div>
+      <div className="count">{count}</div>
+      <button className="increment" onClick={handleButtonClick}>
+        Increment
+      </button>
+    </div>
+  );
+}
