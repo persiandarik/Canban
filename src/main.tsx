f76c3449204b2884515d9d43a24ac394988cf8cd
persiandarik <persiandarik@gmail.com>
2026-02-16 11:45:43 +0330

    [َِِADD] global styles

--- src/main.tsx
+++ src/main.tsx
@@ -3,5 +3,9 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 
 import "./index.css";
+import "./styles/colors.css";
+import "./styles/shadows.css";
+import "./styles/shapes.css";
+import "./styles/typography.css";
 
 createRoot(document.getElementById("root")!).render(<App />);
