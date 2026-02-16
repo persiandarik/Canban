ef48b0d59d2938f042b82a748bcf92f42cb5babe
persiandarik <persiandarik@gmail.com>
2026-02-16 13:38:09 +0330

    [REMOVE] unnecessary code

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,7 @@
 import { type ReactNode } from "react";
 
-import Card from "@/components/Card/Card.tsx";
-import Post from "@/components/Post/Post.tsx";
-
 import styles from "./App.module.css";
 
 export default function App(): ReactNode {
-  return (
-    <div className={styles.app}>
-      <Card />
-      <Post />
-    </div>
-  );
+  return <div className={styles.app}>Hello, friend!</div>;
 }
