5495aeee21ce1b08911341abde4d02dec4bfedb6
persiandarik <persiandarik@gmail.com>
2026-02-19 13:27:55 +0330

    [ADD] board page

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import { type ReactNode } from "react";
 
 import { Route, Routes } from "react-router";
 
+import BoardPage from "@/pages/BoardPage/BoardPage.tsx";
 import HomePage from "@/pages/HomePage/HomePage.tsx";
 
 export default function App(): ReactNode {
   return (
     <Routes>
       <Route index element={<HomePage />} />
+      <Route path="board" element={<BoardPage />} />
     </Routes>
   );
 }
---
+++ src/pages/BoardPage/BoardPage.tsx
@@ -0,0 +1,7 @@
+import type { ReactNode } from "react";
+
+import styles from "./BoardPage.module.css";
+
+export default function BoardPage(): ReactNode {
+  return <div className={styles["board-page"]}>Board Page</div>;
+}
