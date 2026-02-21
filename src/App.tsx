324fc2f3c3ad50edbff47f5082a97e15da9a9c77
persiandarik <persiandarik@gmail.com>
2026-02-21 12:06:18 +0330

    [ADD] root layout

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,18 @@ import { type ReactNode } from "react";
 
 import { Route, Routes } from "react-router";
 
+import RootLayout from "@/layouts/RootLayout/RootLayout.tsx";
+
 import BoardPage from "@/pages/BoardPage/BoardPage.tsx";
 import HomePage from "@/pages/HomePage/HomePage.tsx";
 
 export default function App(): ReactNode {
   return (
     <Routes>
-      <Route index element={<HomePage />} />
-      <Route path="board" element={<BoardPage />} />
+      <Route element={<RootLayout />}>
+        <Route index element={<HomePage />} />
+        <Route path="board" element={<BoardPage />} />
+      </Route>
     </Routes>
   );
 }
--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -2,48 +2,42 @@ import type { ReactNode } from "react";
 
 import BoardCard from "@/components/BoardCard/BoardCard.tsx";
 import Button from "@/components/Button/Button.tsx";
-import Footer from "@/components/Footer/Footer.tsx";
-import Header from "@/components/Header/Header.tsx";
 
 import styles from "./HomePage.module.css";
 
 export default function HomePage(): ReactNode {
   return (
     <div className={styles["home-page"]}>
-      <Header />
-      <main>
-        <div className={styles.header}>
-          <h1>Boards</h1>
-          <Button color="primary">Create</Button>
-        </div>
-        <ul className={styles.boards}>
-          <li>
-            <BoardCard
-              id={1}
-              title="Sprint Tasks"
-              description="A board to keep track of the team's work during each sprint. Tasks move from To Do through In Progress until they reach Done."
-              color="blue"
-            />
-          </li>
-          <li>
-            <BoardCard
-              id={2}
-              title="Content Calendar"
-              description="Plan and manage posts, blogs, or videos ahead of time. Items flow from Idea to Draft and finally to Published for a smooth content pipeline."
-              color="gray"
-            />
-          </li>
-          <li>
-            <BoardCard
-              id={3}
-              title="Personal Goals"
-              description="Organize personal or professional goals into small tasks."
-              color="yellow"
-            />
-          </li>
-        </ul>
-      </main>
-      <Footer />
+      <div className={styles.header}>
+        <h1>Boards</h1>
+        <Button color="primary">Create</Button>
+      </div>
+      <ul className={styles.boards}>
+        <li>
+          <BoardCard
+            id={1}
+            title="Sprint Tasks"
+            description="A board to keep track of the team's work during each sprint. Tasks move from To Do through In Progress until they reach Done."
+            color="blue"
+          />
+        </li>
+        <li>
+          <BoardCard
+            id={2}
+            title="Content Calendar"
+            description="Plan and manage posts, blogs, or videos ahead of time. Items flow from Idea to Draft and finally to Published for a smooth content pipeline."
+            color="gray"
+          />
+        </li>
+        <li>
+          <BoardCard
+            id={3}
+            title="Personal Goals"
+            description="Organize personal or professional goals into small tasks."
+            color="yellow"
+          />
+        </li>
+      </ul>
     </div>
   );
 }
