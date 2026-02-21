809b39924155cd0269b94a73d59534b5d1e6b608
persiandarik <persiandarik@gmail.com>
2026-02-21 14:25:00 +0330

    session 4 (#4)

--- src/App.tsx
+++ src/App.tsx
@@ -1,102 +1,21 @@
-import { type ReactNode, useState } from "react";
+import { type ReactNode } from "react";
 
-import "./App.css";
+import { Route, Routes } from "react-router";
 
-export type Theme = "light" | "dark";
+import RootLayout from "@/layouts/RootLayout/RootLayout.tsx";
 
-type User = {
-  id: number;
-  username: string;
-  role: "user" | "admin";
-};
+import BoardPage from "@/pages/BoardPage/BoardPage.tsx";
+import HomePage from "@/pages/HomePage/HomePage.tsx";
+import NotFoundPage from "@/pages/NotFoundPage/NotFoundPage.tsx";
 
 export default function App(): ReactNode {
-  const [users, setUsers] = useState<User[]>([
-    {
-      id: 1,
-      username: "BijanProgrammer",
-      role: "admin",
-    },
-    {
-      id: 2,
-      username: "Reza",
-      role: "user",
-    },
-    {
-      id: 3,
-      username: "Ali",
-      role: "admin",
-    },
-  ]);
-
-  const handleAddButtonClick = (): void => {
-    setUsers((old) => {
-      const clone = [...old];
-
-      clone.splice(1, 0, {
-        id: clone.length + 1,
-        username: "New",
-        role: "user",
-      });
-
-      return clone;
-    });
-  };
-
   return (
-    <div className="app">
-      <button onClick={handleAddButtonClick}>Add User</button>
-
-      {users.map((user) => (
-        <Item key={user.id} user={user} />
-      ))}
-
-      {users.length % 2 === 0 ? (
-        <Hello key="even" label="even" />
-      ) : (
-        <Hello key="odd" label="odd" />
-      )}
-
-      {users.length % 2 === 0 && <Hello label="even" />}
-      {users.length % 2 === 1 && <Hello label="odd" />}
-    </div>
-  );
-}
-
-type HelloProps = {
-  label: string;
-};
-
-function Hello({ label }: HelloProps): ReactNode {
-  const [name, setName] = useState<string>("");
-
-  return (
-    <div>
-      <label>
-        {label}
-        <input
-          type="text"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-        />
-      </label>
-      <br />
-      <div>Hello, {name}!</div>
-    </div>
-  );
-}
-
-type ItemProps = {
-  user: User;
-};
-
-function Item({ user }: ItemProps): ReactNode {
-  console.log(user.username);
-
-  return (
-    <li>
-      ({user.role}) {user.username}
-      <input type="text" />
-    </li>
+    <Routes>
+      <Route element={<RootLayout />}>
+        <Route index element={<HomePage />} />
+        <Route path="board/:id" element={<BoardPage />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Route>
+    </Routes>
   );
 }
