ea3cf4eb4fe41c8232d10e44559b3bace678afba
persiandarik <persiandarik@gmail.com>
2026-04-16 15:36:39 +0330

    [ADD] counter provider

--- src/pages/BoardPage/BoardPage.tsx
+++ src/pages/BoardPage/BoardPage.tsx
@@ -1,31 +1,17 @@
-import { type ReactNode, useState } from "react";
+import { type ReactNode } from "react";
 
 import Board from "@/components/Board/Board.tsx";
 
-import { CounterContext } from "@/context/counter-context.ts";
+import CounterProvider from "@/providers/CounterProvider.tsx";
 
 import styles from "./BoardPage.module.css";
 
 export default function BoardPage(): ReactNode {
-  const [count, setCount] = useState<number>(0);
-
-  const increment = (): void => {
-    setCount((old) => old + 1);
-  };
-
-  const decrement = (): void => {
-    setCount((old) => old - 1);
-  };
-
-  const reset = (): void => {
-    setCount(0);
-  };
-
   return (
     <div className={styles["board-page"]}>
-      <CounterContext value={{ count, increment, decrement, reset }}>
+      <CounterProvider>
         <Board />
-      </CounterContext>
+      </CounterProvider>
     </div>
   );
 }
