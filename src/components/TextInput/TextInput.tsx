cedf8d842d9642fd23bf33f49e9ea7064aee0235
persiandarik <persiandarik@gmail.com>
2026-04-26 19:57:47 +0330

    Revert "[ADD] controlled input example"
    This reverts commit cc629329918470f73b3082923842b73001623d0d.

--- src/components/TextInput/TextInput.tsx
+++ src/components/TextInput/TextInput.tsx
@@ -1,10 +1,4 @@
-import {
-  type ChangeEvent,
-  type ComponentProps,
-  type ReactNode,
-  useId,
-  useState,
-} from "react";
+import { type ComponentProps, type ReactNode, useId } from "react";
 
 import clsx from "clsx";
 
@@ -23,31 +17,12 @@ export default function TextInput({
 }: Props): ReactNode {
   const id = useId();
 
-  const [value, setValue] = useState<string>("");
-
-  const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
-    const cleaned = e.target.value.replace(/\D/g, "");
-
-    if (cleaned === "") {
-      setValue("");
-      return;
-    }
-
-    const n = Number.parseInt(cleaned);
-    setValue(n.toLocaleString());
-  };
-
   return (
     <div
       className={clsx(styles["text-input"], !!error && styles.error, className)}
     >
       <label htmlFor={id}>{label}</label>
-      <input
-        id={id}
-        {...otherProps}
-        value={value}
-        onChange={handleInputChange}
-      />
+      <input id={id} {...otherProps} />
       <span className={styles.error}>{error || "\u00A0"}</span>
     </div>
   );
