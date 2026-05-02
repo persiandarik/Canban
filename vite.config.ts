ae46cf2a5a2271c959099991b205df042154dedd
persiandarik <persiandarik@gmail.com>
2026-05-02 10:11:22 +0330

    [ADD] react compiler

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,13 @@ import path from "node:path";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react({
+      babel: {
+        plugins: ["babel-plugin-react-compiler"],
+      },
+    }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
