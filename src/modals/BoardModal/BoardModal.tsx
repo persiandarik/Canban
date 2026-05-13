7623bb1bf78f8476735afaca5cfef5aaa7f26cf1
persiandarik <persiandarik@gmail.com>
2026-05-13 16:07:22 +0330

    [FIX] default color not being applied

--- src/modals/BoardModal/BoardModal.tsx
+++ src/modals/BoardModal/BoardModal.tsx
@@ -41,7 +41,7 @@ export default function BoardModal({
     reset,
     formState: { errors },
   } = useForm({
-    defaultValues,
+    defaultValues: defaultValues ?? { color: "blue" },
     resolver: zodResolver(BoardSchema),
   });
 
